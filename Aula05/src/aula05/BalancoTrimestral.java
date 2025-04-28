932d056b5fb8c966c628c03e1f9d460fb2676b4f
Murilo Rodrigues <muriloveroneze0987@gmail.com>
2025-04-28 08:54:17 -0300

    Aula de java mno NetBeans

---
+++ Aula05/src/aula05/BalancoTrimestral.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+    
+/**
+ *
+ * @author CAMARGO
+ */
+public class BalancoTrimestral {
+    
+    public static void main(String[] args) {
+        
+        int GastosJan = 15000;
+        int GastosFev = 23000;
+        int GastosMar = 17000;
+        
+        int gastoTrimestre = GastosJan + GastosFev + GastosMar;
+        
+        System.out.println("Os gastos do trimestre " + gastoTrimestre);
+        
+        
+        
+    }
+    
+}
