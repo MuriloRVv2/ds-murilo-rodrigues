505bd5473f061298b14a8b7091a6c2f1c105af96
Murilo Rodrigues <muriloveroneze0987@gmail.com>
2025-08-25 09:30:05 -0300

    Aula de Spring Boot(IntelliJ)

---
+++ Aula202/src/main/java/com/spring/Aula202/Aula202Application.java
@@ -0,0 +1,16 @@
+package com.spring.Aula202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula202Application.class, args);
+
+
+
+	}
+
+}
